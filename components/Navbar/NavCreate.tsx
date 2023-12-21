4e736a4c4aebeaa21d265e26b7bf63c3ab2fe3e2
Jack Roybal <excusemeimjack@gmail.com>
2023-12-20 22:52:56 -0800

    Set up profile pages

--- components/Navbar/NavCreate.tsx
+++ components/Navbar/NavCreate.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function NavCreate() {
   return (
-    <div>
-
+    <div className="dropdown">
+      <button tabIndex={0} role="button" className="flex items-center gap-1 bg-[#A155B9] hover:bg-[#7f4291] text-[#ffffff] py-1 px-3 rounded-xl" >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" dataSlot="icon" className="w-5 h-5">
+          <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+        </svg>
+        <h1 className=''>CREATE</h1>
+      </button>
+      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-[#212022] rounded-box w-48 mt-2 border-2 border-[#DEDEDE]">
+        <li><a>Review</a></li>
+        <li><a>List</a></li>
+      </ul>
     </div>
   )
 }
--- components/Navbar/UserProfileDropdown.tsx
+++ components/Navbar/UserProfileDropdown.tsx
@@ -2,6 +2,7 @@ import { signOut, useSession } from 'next-auth/react'
 import React, { useEffect, useState } from 'react'
 import Loading from '../Loading';
 import { BarLoader } from 'react-spinners';
+import Link from 'next/link';
 
 function UserProfileDropdown() {
   const [user, setUser] = useState(null);
@@ -27,12 +28,12 @@ function UserProfileDropdown() {
     getUser();
   }, [status])
 
-  if (status === 'loading' || !user || !session) return <Loading loader={2}/>;
+  if (status === 'loading' || !user || !session) return <Loading loader={2} />;
 
   const currUser = Object.values(user)[0];
   const { username, profile_pic_url } = currUser;
 
-  if (!currUser || !username || !profile_pic_url) return <Loading loader={2}/>;
+  if (!currUser || !username || !profile_pic_url) return <Loading loader={2} />;
 
   return (
     <div className="dropdown">
@@ -43,12 +44,17 @@ function UserProfileDropdown() {
           </div>
         </div>
         {username.toUpperCase()}
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" dataSlot="icon" className="w-5 h-5">
+            <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+          </svg>
+        </div>
       </div>
       <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-[#212022] rounded-box w-48 mt-2 border-2 border-[#DEDEDE]">
-        <li><a>Profile</a></li>
-        <li><a>Watchlist</a></li>
-        <li><a>Lists</a></li>
-        <li><a>Friends</a></li>
+        <li><Link href="/profile">Profile</Link></li>
+        <li><Link href="/profile/watchlist">Watchlist</Link></li>
+        <li><Link href="/profile/lists">Lists</Link></li>
+        <li><Link href="/profile/friends">Friends</Link></li>
         <li><div onClick={() => signOut()}>Sign Out</div></li>
       </ul>
     </div>
