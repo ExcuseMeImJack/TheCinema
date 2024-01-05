65be93dcfbf23c3cdbbf69f1ca2fc76254ac14ce
Jack Roybal <excusemeimjack@gmail.com>
2024-01-05 13:16:12 -0800

    User api completed

---
+++ app/api/users/getCurrUserFriends/route.ts
@@ -0,0 +1,31 @@
+import { authOptions } from "../../auth/[...nextauth]/route";
+import prisma from '@/lib/prisma';
+import { NextResponse } from "next/server";
+import { Session, getServerSession } from "next-auth";
+
+export async function GET(req: Request) {
+  try {
+    const session = (await getServerSession(authOptions)) as Session | null;
+
+    if (!session || !session.user || !session.user.email) {
+      return NextResponse.json(null, { status: 200 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        Friends: true
+      }
+    });
+
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
