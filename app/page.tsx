3ea05fe57108975e179104992231085da84fcf6d
Jack Roybal <excusemeimjack@gmail.com>
2024-06-17 18:01:56 -0700

    Removed same image prevention

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,13 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [filmBackground, setFilmBackground] = useState("");
-  const[prevFilmBackground, setPrevFilmBackground] = useState("");
 
   useEffect(() => {
     const interval = setInterval(async () => {
-      if(filmBackground !== prevFilmBackground) {
-        setPrevFilmBackground(filmBackground);
         setFilmBackground(await getRandomFilmBackground());
-      } else {
-        setFilmBackground(await getRandomFilmBackground());
-      }
     }, 15000)
     return () => clearInterval(interval);
-  }, []);
+  }, [filmBackground]);
 
   return (
     <div className="flex flex-col h-full">
