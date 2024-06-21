59db28296d537bd309e37ba07c66b0ecbc037028
Jack Roybal <excusemeimjack@gmail.com>
2024-06-21 15:31:07 -0700

    Added dynamic Film Details Pages

---
+++ app/films/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FilmDetails({ params }: { params: { id: Number } }) {
+  const filmID = params.id;
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default FilmDetails
--- app/films/page.tsx
+++ app/films/page.tsx
@@ -6,10 +6,12 @@ import Image from 'next/image';
 import './films.css'
 import React, { useEffect, useState } from 'react'
 import { formatDate } from '../../lib/utils/formatDate';
+import { useRouter } from 'next/navigation';
 
 function Films() {
 
   const [films, setFilms] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchFilms = async () => {
@@ -37,13 +39,21 @@ function Films() {
     return rating.toFixed(1) + "/10";
   }
 
+  const openFilmDetails = (filmID) => {
+    router.refresh();
+    router.push(`/films/${filmID}`);
+  }
+
   if (!films || !films.length) return <Loading loader={1} />
   return (
     <div className='mx-16'>
       <h1 className='text-4xl font-HeaderFont font-bold text-center m-16'>FILMS</h1>
       <div className='films_container flex flex-wrap justify-evenly items-center gap-4'>
         {films.map((film, i) => (
-          <div className="card w-48 bg-base-100 shadow-xl border-2 rounded-lg hover:border-[var(--interactHover)] hover:cursor-pointer" key={i}>
+          <div
+            className="card w-48 bg-base-100 shadow-xl border-2 rounded-lg hover:border-[var(--interactHover)] hover:cursor-pointer"
+            key={i}
+            onClick={() => openFilmDetails(film.id)}>
             <div className='tinted-div'>
               <Image
                 className='rounded-md'
@@ -53,11 +63,11 @@ function Films() {
               />
               <div className="text-overlay flex flex-col gap-3 justify-evenly">
                 <div>
-                  <p className='font-bold'>{film.title}</p>
-                  <p className='font-bold text-gray-400'>{film.release_date.split('-')[0]}</p>
+                  <p className='font-bold text-lg'>{film.title}</p>
+                  <p className='font-bold text-md text-gray-400'>{film.release_date.split('-')[0]}</p>
                 </div>
-                <p>{film.overview.length > 100 ? (`${film.overview.slice(0, 100)}...`) : film.overview}</p>
-                <p className='font-bold text-[var(--highlight)]'>{getRatingStatus(film)}</p>
+                <p className='text-sm'>{film.overview.length > 180 ? (`${film.overview.slice(0, 180)}...`) : film.overview}</p>
+                <p className='font-bold text-sm text-[var(--highlight)]'>{getRatingStatus(film)}</p>
               </div>
             </div>
           </div>
