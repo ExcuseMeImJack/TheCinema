0e9b7ddecaf029918964fe67af017b19f00b54aa
Jack Roybal <excusemeimjack@gmail.com>
2024-07-24 14:15:50 -0700

    Navbar adjustments

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -56,33 +56,36 @@ const Films: React.FC = () => {
   return (
     <div className='mx-16'>
       <h1 className='text-4xl font-HeaderFont font-bold text-center mx-16 mt-16 mb-6'>FILMS</h1>
-      <SearchBar searchType={"film"} setSearchedItems={setFilms} setIsLoading={setIsLoading}/>
       {films.length === 0 || isLoading ? <Loading loader={1} /> :
-      <div className='films_container flex flex-wrap justify-evenly items-center gap-4'>
-        {films.map((film) => (
-          <div
-            className="card w-48 bg-base-100 shadow-xl border-2 rounded-lg hover:border-[var(--interactHover)] hover:cursor-pointer"
-            key={film.id}
-            onClick={() => openFilmDetails(film.id)}>
-            <div className='tinted-div'>
-              <Image
-                className='rounded-md'
-                src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt={`Film Poster: ${film.title}`}
-                width={400}
-                height={600}
-              />
-              <div className="text-overlay flex flex-col gap-3 justify-evenly">
-                <div>
-                  <p className='font-bold text-lg'>{film.title}</p>
-                  <p className='font-bold text-md text-gray-400'>{film.release_date.split('-')[0]}</p>
+        // Work on making justify-center to justify-start
+        <div className='flex flex-col'>
+          <SearchBar searchType={"film"} setSearchedItems={setFilms} setIsLoading={setIsLoading} />
+          <div className={`films_container flex flex-wrap justify-evenly gap-4`}>
+            {films.map((film) => (
+              <div
+                className="card w-48 bg-base-100 shadow-xl border-2 rounded-lg hover:border-[var(--interactHover)] hover:cursor-pointer"
+                key={film.id}
+                onClick={() => openFilmDetails(film.id)}>
+                <div className='tinted-div'>
+                  <Image
+                    className='rounded-md'
+                    src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt={`Film Poster: ${film.title}`}
+                    width={400}
+                    height={600}
+                  />
+                  <div className="text-overlay flex flex-col gap-3 justify-evenly">
+                    <div>
+                      <p className='font-bold text-lg'>{film.title}</p>
+                      <p className='font-bold text-md text-gray-400'>{film.release_date.split('-')[0]}</p>
+                    </div>
+                    <p className='text-sm'>{film.overview.length > 125 ? (`${film.overview.slice(0, 125)}...`) : film.overview}</p>
+                    <p className='font-bold text-sm text-[var(--highlight)]'>{getRatingStatus(film)}</p>
+                  </div>
                 </div>
-                <p className='text-sm'>{film.overview.length > 125 ? (`${film.overview.slice(0, 125)}...`) : film.overview}</p>
-                <p className='font-bold text-sm text-[var(--highlight)]'>{getRatingStatus(film)}</p>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>}
+        </div>}
     </div>
   )
 }
